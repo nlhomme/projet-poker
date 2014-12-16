40ed8fa3ebe777d6b10b39ec786b264e578bcca7
BAUDON Florent <florent.baudon@gmail.com>
2014-12-16 09:06:15 +0100

    Reparation copilation

--- Texas_hold_em/src/main.cpp
+++ Texas_hold_em/src/main.cpp
@@ -1,38 +1,33 @@
-#include "../SDL2/include/SDL.h"
+#if defined (WIN32)
+
+    #include <winsock2.h>
+
+    typedef int socklen_t;
+
+#elif defined (linux)
+
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h>
+
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define closesocket(s) close (s)
+
+    typedef int SOCKET;
+    typedef struct sockaddr_in SOCKADDR_IN;
+    typedef struct sockaddr SOCKADDR;
+
+#endif
 
 #include <stdio.h>
+#include <stdlib.h> 
 
 int main(int argc, char *argv[])
 {
- /* Initialisation simple */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
-    {
-        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
-        return -1;
-    }
-
-    {
-        /* Création de la fenêtre */
-        SDL_Window* pWindow = NULL;
-        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                                  SDL_WINDOWPOS_UNDEFINED,
-                                                                  640,
-                                                                  480,
-                                                                  SDL_WINDOW_SHOWN);
-
-        if( pWindow )
-        {
-            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
-
-            SDL_DestroyWindow(pWindow);
-        }
-        else
-        {
-            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
-        }
-    }
-
-    SDL_Quit();
+	printf("Hello");
 
     return 0;
 }
